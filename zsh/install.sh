5e94978ab4a425b0c6eb75298aea49ab95d3ea6e
Joonas Meriläinen <merilainenjoonas@gmail.com>
2016-01-31 11:13:31 +0200

    init

---
+++ zsh/install.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+#
+# oh-my-zsh
+#
+
+# Check ~/.oh-my-zsh
+if [ -d "~/.oh-my-zsh" ]
+then
+  echo "  Installing oh-my-zsh for you."
+
+  if test "$(uname)" = "Darwin"
+  then
+    sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+  fi
+
+fi
+
+exit 0
+
+chsh -s /usr/local/bin/zsh
