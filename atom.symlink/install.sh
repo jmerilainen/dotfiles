b2f67cfbcffb6dab92f83ad6b4275dcd89286b2d
Joonas Meriläinen <merilainenjoonas@gmail.com>
2016-02-02 22:06:09 +0200

    init

---
+++ atom.symlink/install.sh
@@ -0,0 +1,14 @@
+apm install file-icons
+apm install atom-project-manager
+apm install atom-minimap
+apm install merge-conflicts
+apm install linter
+apm install linter-csslint
+apm install linter-htmlint
+apm install linter-jslint
+apm install linter-less
+apm install linter-php
+apm install emmet
+apm install color-picker
+apm install base16-ocean-dark-syntax-theme
+apm install atom-material-ui
