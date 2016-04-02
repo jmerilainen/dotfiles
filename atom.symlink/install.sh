37349c5406e3081d5990864a794b3ff8bd365008
Joonas Meriläinen <merilainenjoonas@gmail.com>
2016-04-02 12:22:04 +0300

    Atom configs

--- atom.symlink/install.sh
+++ atom.symlink/install.sh
@@ -1,10 +1,7 @@
 apm install file-icons
-apm install atom-project-manager
-apm install atom-minimap
 apm install merge-conflicts
 apm install linter
 apm install linter-csslint
-apm install linter-htmlint
 apm install linter-jslint
 apm install linter-less
 apm install linter-php
